694811fbbe55f401fe28fb056a51370bd4a7f76a
callmewaggs <zxv74014@gmail.com>
2019-09-30 17:23:43 +0900

    0930

---
+++ Udacity-Web_Services_And_APIs/graphql/src/main/resources/data.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS dog;
+
+CREATE TABLE dog
+(
+    id     LONG AUTO_INCREMENT PRIMARY KEY,
+    name   VARCHAR(250) NOT NULL,
+    breed  VARCHAR(250) NOT NULL,
+    origin VARCHAR(250)
+);
+
+INSERT INTO dog (id, name, breed, origin)
+VALUES (1, 'MAX', 'Basque Shepherd Dog', 'South Korea');
+INSERT INTO dog (id, name, breed, origin)
+VALUES (2, 'FAX', 'Combai', 'Japan');
+INSERT INTO dog (id, name, breed, origin)
+VALUES (3, 'PINEAPPLE', 'German Wirehaired Pointer', 'Germany');
+INSERT INTO dog (id, name, breed, origin)
+VALUES (4, 'MANGO', 'Kerry Beagle', 'India');
+INSERT INTO dog (id, name, breed, origin)
+VALUES (5, 'ROOKIE', 'Old English Terrier', 'England');
+INSERT INTO dog (id, name, breed, origin)
+VALUES (6, 'HONGKI', 'Neighborhood Dog', 'North Korea');
+INSERT INTO dog (id, name, breed, origin)
+VALUES (7, 'CAPTAIN', 'American Express', 'United States of America');
